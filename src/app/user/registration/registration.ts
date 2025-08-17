b55d1f7716b8ae246d4f8c4b844cad21ec7719ab
Muhammad Danish <danishkanirya9999@gmail.com>
2025-08-18 00:19:22 +0500

    adding validation to form

--- src/app/user/registration/registration.ts
+++ src/app/user/registration/registration.ts
@@ -1,27 +1,47 @@
-import { Component } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
-  standalone:true,
-  imports: [ReactiveFormsModule],
+  standalone: true,
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './registration.html',
   styleUrl: './registration.css'
 })
-export class Registration {
-  form: ReturnType<FormBuilder['group']>; 
+export class Registration implements OnInit {
+  form!: ReturnType<FormBuilder['group']>;
+  isSubmitted: boolean = false;
 
-  constructor(public formBuilder: FormBuilder) {
+  constructor(public formBuilder: FormBuilder) {}
+
+  ngOnInit() {
     this.form = this.formBuilder.group({
-      fullName: [''],
-      email:[''],
-      password:[''],
-      confirmPassword:['']
-
-    });
+      fullName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['',
+        [Validators.required, Validators.minLength(6), Validators.pattern(/(?=.*[^a-zA-Z0-9 ])/)]
+      ],
+      confirmPassword: ['']
+    }, { validators: this.passwordMatchValidator });
   }
 
-  onSubmit(){
+  passwordMatchValidator: ValidatorFn = (control: AbstractControl): null => {
+    const password = control.get('password')
+    const confirmPassword = control.get('confirmPassword')
+    if (password && confirmPassword && password.value != confirmPassword.value)
+      confirmPassword?.setErrors({ passwordMismatch: true })
+    else
+      confirmPassword?.setErrors(null);
+    return null;
+  }
+
+  hasDisplayableError(controlName: string): Boolean {
+    const control = this.form.get(controlName);
+    return Boolean(control?.invalid)&&(this.isSubmitted||Boolean(control?.touched))
+  }
+
+  onSubmit() {
     console.log(this.form.value);
   }
 }
